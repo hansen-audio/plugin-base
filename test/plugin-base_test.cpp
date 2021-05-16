2d92f489d6a83c06e15f3c08c1eedd8f0103833d
René Hansen <rene@hansen-audio.de>
2021-05-16 11:51:04 +0200

    Remove non existant header

--- test/plugin-base_test.cpp
+++ test/plugin-base_test.cpp
@@ -1,9 +1,5 @@
 #include "gtest/gtest.h"
 
-#include "ha/plugin_base/tools.h"
-
-using namespace ha::plugin_base;
-
 namespace {
 
 //-----------------------------------------------------------------------------
